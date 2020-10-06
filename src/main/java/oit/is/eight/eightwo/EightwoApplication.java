e85cce39fb9259972cf68dcc896ec58d4c7f595e
e1b18026 <e1b18026@oit.ac.jp>
2020-10-06 16:47:24 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/eight/eightwo/EightwoApplication.java
@@ -0,0 +1,13 @@
+package oit.is.eight.eightwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EightwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EightwoApplication.class, args);
+	}
+
+}
